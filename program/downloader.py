8e0642cf0887f705f299d99cc84de71afe7ae363
salamdjdjfj <98358954+salamdjdjfj@users.noreply.github.com>
2022-03-19 02:54:53 +0400

    update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -60,7 +60,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎧 @{bn} tərəfindən yükləndi.\n\n⚡ Kanalımıza abunə olmağı unutmayın @SnactusNews**"
+        rep = f"**🎧 @{bn} tərəfindən yükləndi.\n\n⚡ Kanalımıza abunə olmağı unutmayın @teeamabasofinfo**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
